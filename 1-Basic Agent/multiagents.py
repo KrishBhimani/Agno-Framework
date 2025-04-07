d6d1d9d4a6302b89e394f663cf3a34ce1931bdc1
KrishBhimani <erkrishbhimani@gmail.com>
2025-04-07 21:24:53 +0530

    updated

--- 1-Basic Agent/multiagents.py
+++ 1-Basic Agent/multiagents.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD:1-Basic Agents/multiagents.py
+
 from agno.agent import Agent
 from agno.models.groq import Groq
 from agno.embedder.huggingface import HuggingfaceCustomEmbedder
@@ -42,91 +41,4 @@ agent_team = Agent(
 )
 query="What are the analyst recommendations for Amazon and Netflix? Should I invest in them?"
 agent_team.print_response(query, stream=True)
-=======
-from agno.agent import Agent
-from agno.models.groq import Groq
-from agno.embedder.huggingface import HuggingfaceCustomEmbedder
-from agno.tools.duckduckgo import DuckDuckGoTools
-from agno.tools.yfinance import YFinanceTools
 
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
-os.environ["HF_TOKEN"]=os.getenv("HF_TOKEN")
-
-web_agent = Agent(
-    name="Web Agent",
-    role="Search the web for information",
-    model=Groq(id="qwen-2.5-32b"),
-    tools=[DuckDuckGoTools()],
-    instructions="Always include sources",
-    show_tool_calls=True,
-    markdown=True,
-)
-
-finance_agent = Agent(
-    name="Finance Agent",
-    role="Get financial data",
-    model=Groq(id="qwen-2.5-32b"),
-    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True,stock_fundamentals=True,company_info=True)],
-    instructions="Use tables to display data",
-    show_tool_calls=True,
-    markdown=True,
-)
-
-agent_team = Agent(
-    team=[web_agent, finance_agent],
-    model=Groq(id="qwen-2.5-32b"),
-    instructions=["Always include sources", "Use tables to display data"],
-    show_tool_calls=True,
-    markdown=True,
-)
-query="What are the analyst recommendations for Amazon and Netflix? Should I invest in them?"
-agent_team.print_response(query, stream=True)
->>>>>>> 40e5da559f18573330e2aba3a47e36dfbda458df:1-Basic Agent/multiagents.py
-=======
-from agno.agent import Agent
-from agno.models.groq import Groq
-from agno.embedder.huggingface import HuggingfaceCustomEmbedder
-from agno.tools.duckduckgo import DuckDuckGoTools
-from agno.tools.yfinance import YFinanceTools
-
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
-os.environ["HF_TOKEN"]=os.getenv("HF_TOKEN")
-
-web_agent = Agent(
-    name="Web Agent",
-    role="Search the web for information",
-    model=Groq(id="qwen-2.5-32b"),
-    tools=[DuckDuckGoTools()],
-    instructions="Always include sources",
-    show_tool_calls=True,
-    markdown=True,
-)
-
-finance_agent = Agent(
-    name="Finance Agent",
-    role="Get financial data",
-    model=Groq(id="qwen-2.5-32b"),
-    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True,stock_fundamentals=True,company_info=True)],
-    instructions="Use tables to display data",
-    show_tool_calls=True,
-    markdown=True,
-)
-
-agent_team = Agent(
-    team=[web_agent, finance_agent],
-    model=Groq(id="qwen-2.5-32b"),
-    instructions=["Always include sources", "Use tables to display data"],
-    show_tool_calls=True,
-    markdown=True,
-)
-query="What are the analyst recommendations for Amazon and Netflix? Should I invest in them?"
-agent_team.print_response(query, stream=True)
->>>>>>> 09cb6032527599c31bca76077440da88c03ead9d
